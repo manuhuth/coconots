854787643f5b07db2c4dd8b1d574de7ab395514d
manuhuth <s6mahuth@uni-bonn.de>
2023-03-01 20:33:24 +0100

    Add first test.

--- tests/testthat/test-coconots.R
+++ tests/testthat/test-coconots.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("GP2Works", {
+  length <- 1000
+  par <- c(0.5,0.2,0.05,0.3,0.3)
+  data.sim <- cocoSim(order = 2, type = "GP", par = par, length = length)
+  data <- data.sim$data
+  fit <- cocoReg(order = 2, type = "GP", data = data)
 })
