d7a2fc3a7fc92399fbf51b0746d5fe8080091cc4
RCJung <robert.jung@uni-hohenheim.de>
2024-12-09 12:25:47 +0100

    Update plot.cocoForecast.R
    Adjusted Title and labels

--- R/plot.cocoForecast.R
+++ R/plot.cocoForecast.R
@@ -6,7 +6,7 @@ autoplot.cocoForecast <- function(object, breaks=NULL, width=0.1, ...){
   
   pl <- ggplot2::ggplot(mapping = ggplot2::aes(x = object$x, y = object$densities_plot)) +
       ggplot2::geom_bar(stat="identity", position="dodge", width=width) + 
-      ggplot2::labs(title = "Probability mass forecast", x = "Support", y = "Probability mass") +
+      ggplot2::labs(title = "Density forecast", x = "Support", y = "Probability") +
     ggplot2::xlim(c(0, max(object$x))) +
       ggplot2::theme_bw() + ggplot2::theme(text = ggplot2::element_text(size = 20)) 
 
